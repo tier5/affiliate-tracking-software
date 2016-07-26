cedb20bb3fd2053c2862f8bebfdf299821cce192
Scott Conrad <scott@honopu.com>
2016-07-26 17:54:25 -0400

    cleanup

--- admin/app/controllers/ReviewController.php
+++ admin/app/controllers/ReviewController.php
@@ -12,9 +12,6 @@ use Vokuro\Models\Users;
 
 use Services_Twilio;
 use Services_Twilio_RestException;
-
-//TURN ON PRETTY ERRORS!!!
-error_reporting(E_ALL);
 ini_set("display_errors","on");
 
 class ReviewController extends ControllerBase
@@ -41,7 +38,7 @@ class ReviewController extends ControllerBase
       // Perform the query
       $review_invite = new ReviewInvite();
       $invite = $review_invite::findFirst(array($conditions, "bind" => $parameters));
-        
+
       if ($invite->location_id > 0) {
 
         //we have the invite, now find the location
@@ -56,16 +53,15 @@ class ReviewController extends ControllerBase
         $this->view->logo_setting = $location->sms_message_logo_path;
         $this->view->name = $location->name;
         //echo '<pre>$agency:'.print_r($agency,true).'</pre>';
-          
+
         $this->view->setVar('agency', $agency);
-          
+
         if ($invite->review_invite_type_id == 2) {
           $threshold = $location->rating_threshold_star;
         } else {
           $threshold = $location->rating_threshold_nps;
         }
         $this->view->setVar('threshold', $threshold);
-
 
         //find what type of question we should ask
         $question_type = 1;
@@ -82,12 +78,9 @@ class ReviewController extends ControllerBase
           if (strpos($ref, 'google') !== FALSE) {
              //redirect to wherever google people should go
           } else {
-            if ($this->validateGoogleBotIP($_SERVER['REMOTE_ADDR'])) {
-            } else {
+            if ($this->getIpMatchForGoogleBot($_SERVER['REMOTE_ADDR']) == false){
               //save when the user viewed this invite
-              //and the type when viewed
               $invite->date_viewed = date('Y-m-d H:i:s');
-              //$invite->comments = 'gethostbyaddr:'.gethostbyaddr($_SERVER['REMOTE_ADDR']);
               $invite->review_invite_type_id = $question_type;
               $invite->save();
             }
@@ -99,9 +92,13 @@ class ReviewController extends ControllerBase
 
 
 
-    function validateGoogleBotIP($ip) {
+    public function validateGoogleBotIP($ip) {
       $hostname = gethostbyaddr($ip); //"crawl-66-249-66-1.googlebot.com"
       return preg_match('/\.google\.com$/i', $hostname);
+    }
+
+    public function getIpMatchForGoogleBot($ip){
+      return $this->validateGoogleBotIP($ip);
     }
 
     public function recommendAction()
@@ -133,15 +130,15 @@ class ReviewController extends ControllerBase
           $this->view->sms_button_color = $location->sms_button_color;
           $this->view->logo_setting = $location->sms_message_logo_path;
           $this->view->name = $location->name;
-        
+
           if ($invite->review_invite_type_id == 2) {
             $threshold = $location->rating_threshold_star;
           } else {
             $threshold = $location->rating_threshold_nps;
           }
 
-          
-        
+
+
           //find the location review sites
           $conditions = "location_id = :location_id: AND is_on = 1";
           $parameters = array("location_id" => $invite->location_id);
@@ -165,33 +162,31 @@ class ReviewController extends ControllerBase
         // Perform the query
         $review_invite = new ReviewInvite();
         $invite = $review_invite::findFirst(array($conditions, "bind" => $parameters));
-        
+
         //save the rating
         $invite->rating = $rating;
         $invite->recommend = 'Y';
         $invite->save();
-
-        //echo '<pre>$invite:'.print_r($invite,true).'</pre>';
         //we have the invite, now find the location
         $locationobj = new Location();
         $location = $locationobj::findFirst($invite->location_id);
-        
+
         $this->view->setVar('invite', $invite);
         $this->view->setVar('location', $location);
 
       } catch (\Exception $e) {
-        echo get_class($e), ": ", $e->getMessage(), "\n";
+        echo "Exception: ", $e->getMessage(), "\n";
         echo " File=", $e->getFile(), "\n";
         echo " Line=", $e->getLine(), "\n";
         echo $e->getTraceAsString();
       }
     }
-    
+
 
     public function nothanksAction()
     {
-      
-      $rating = (isset($_GET["r"])?htmlspecialchars($_GET["r"]):'');
+
+      $rating = ($_GET["r"]) ? htmlspecialchars($_GET["r"]) : '';
       // Query review_invite binding parameters with string placeholders
       $conditions = "api_key = :api_key:";
 
@@ -206,7 +201,7 @@ class ReviewController extends ControllerBase
       $invite->recommend = 'N';
       $invite->save();
       $this->view->setVar('invite', $invite);
-      
+
       //we have the invite, now find the location
       $locationobj = new Location();
       $location = $locationobj::findFirst($invite->location_id);
@@ -223,7 +218,7 @@ class ReviewController extends ControllerBase
         //update the comments
         $invite->comments = htmlspecialchars($_POST["comments"]);
         $invite->save();
-        
+
         // Query review_invite binding parameters with string placeholders
         $conditions = "api_key = :api_key:";
         // Parameters whose keys are the same as placeholders
@@ -231,7 +226,7 @@ class ReviewController extends ControllerBase
         // Perform the query
         $review_invite = new ReviewInvite();
         $invite = $review_invite::findFirst(array($conditions, "bind" => $parameters));
-        
+
         //send the notification about the feedback
         $message = 'Notification: Review invite feedback has been posted for '.$location->name.': http://'.$_SERVER['HTTP_HOST'].'/reviews/';
         parent::sendFeedback($agency, $message, $location->location_id, 'Notification: Review invite feedback', $invite->sent_by_user_id);
@@ -240,7 +235,7 @@ class ReviewController extends ControllerBase
     }
 
 
-    
+
     public function trackAction()
     {
       $review_invite_id = $_GET['i'];
@@ -250,11 +245,11 @@ class ReviewController extends ControllerBase
       $rirs->review_invite_id = $review_invite_id;
       $rirs->review_site_id = $review_site_id;
       $rirs->save();
-      
+
       $this->view->disable();
       echo 'true';
     }
-    
+
     public function linkAction()
     {
       // Query review_invite binding parameters with string placeholders
@@ -266,24 +261,15 @@ class ReviewController extends ControllerBase
       // Perform the query
       $review_invite = new ReviewInvite();
       $invite = $review_invite::findFirst(array($conditions, "bind" => $parameters));
-      
-      $ref = '';
-      //if (isset($_SERVER['HTTP_REFERER'])) $ref = $_SERVER['HTTP_REFERER'];
-      //if (strpos($ref, 'google') !== FALSE) {
-          //redirect to wherever google people should go
-      //} else {
         if ($this->validateGoogleBotIP($_SERVER['REMOTE_ADDR'])) {
-          //echo '<p>Testg</p>';
         } else {
           //save when the user viewed this invite
           $invite->date_viewed = date('Y-m-d H:i:s');
           $invite->save();
         }
-      //}
 
       $this->response->redirect($invite->link);
       $this->view->disable();
-      return;
     }
 
 }
