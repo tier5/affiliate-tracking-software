2e51bfd9dae4fd92961f426379bea031e3d4907b
Scott Conrad <scott@honopu.com>
2016-08-11 11:51:41 -0400

    tweak

--- admin/app/models/BaseModel.php
+++ admin/app/models/BaseModel.php
@@ -61,7 +61,7 @@
 
         public function findOneBy($params){
             $records = $this->findwhere($params);
-            if($records) $first = $records[0];
+            if($records && isset($records[0])) $first = $records[0];
             return $first;
         }
 
