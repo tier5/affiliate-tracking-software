6701d4055db97ea3f11c8f15b138b46359750032
Kevin Stotz <kevins@reputationloop.com>
2016-10-24 19:36:13 -0500

    fixed the crong job

--- admin/cron/cron_job_employee_leaderboard.php
+++ admin/cron/cron_job_employee_leaderboard.php
@@ -3,7 +3,7 @@
     use Phalcon\Mvc\Model\Resultset\Simple as Resultset;
     $Start = date("Y-m-01", strtotime('now'));
     $End = date("Y-m-t", strtotime('now'));
-    $sql = "SELECT agency_id FROM agency WHERE parent_id <= 1";
+    $sql = "SELECT agency_id FROM agency WHERE parent_id <= 100";
     $list = new \Vokuro\Models\Agency();
 
     $params = null;
@@ -17,9 +17,15 @@
       $objBusiness = \Vokuro\Models\Agency::findFirst("agency_id = {$BusinessID}");
       $objAgency = \Vokuro\Models\Agency::findFirst("agency_id = " . $objBusiness->parent_id);
 
+
       foreach($dbLocations as $objLocation) {
+      	 echo $objLocation->location_id;
           $dbEmployees = \Vokuro\Models\Users::getEmployeeListReport($BusinessID, $Start, $End, $objLocation->location_id, 0, 0, 1);
           $objEmail = new \Vokuro\Services\Email();
+          echo count($dbEmployees);
           $objEmail->sendEmployeeReport($dbEmployees, $objBusiness);
+          //print_r( $dbEmployees);
+          die;
+          
       }
 }
