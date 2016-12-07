85fe37a51680f96889e4f94fc5c31e66f27454ac
Gary Gipson <Gary Gipson>
2016-12-07 16:13:59 -0600

    BUGFIX:  Was including wrong layout for sales page

--- admin/app/controllers/IndexController.php
+++ admin/app/controllers/IndexController.php
@@ -112,6 +112,7 @@ class IndexController extends ControllerBase {
                     $this->view->logo_path = !empty($agency->logo_path) ? '/img/agency_logos/'.$agency->logo_path : '';
                 }
 
+                $this->view->setTemplateBefore('agencysignup');
                 $this->view->pick('agencysignup/sales');
                 return;
 
