26162830a4bce9a30787060c9c1712231f40abf7
Tier5 <github@tier5.us>
2016-12-13 15:35:22 +0530

    You are able to still submit the form even though there is a password mismatch

--- admin/public/js/signup.js
+++ admin/public/js/signup.js
@@ -1,5 +1,9 @@
 
 $(document).ready(function() {
+
+	var flag1 = false;
+	var flag2 = false;
+
 	if ((document.getElementById('email'))) {
 		document.getElementById('email').addEventListener("focusout", checkAvailableEmail);
 	}
@@ -35,7 +39,16 @@ $(document).ready(function() {
 	
 
 	function submitForm() {
-		document.getElementById('register-form').submit();
+
+		
+
+		checkAvailableEmail();
+	 	//passwordVerification();
+
+			
+
+		
+		
 	}
 	
 	function checkAvailableEmail() {
@@ -44,38 +57,90 @@ $(document).ready(function() {
 		var return_field_object = document.getElementById(return_field_name);
 		var email_field_name = 'email';
 		var email_field_object = document.getElementById(email_field_name);
+
 		
-		if ( !validEmail(email_field_object.value) ) { 
+		if ( !validEmail(email_field_object.value) ) {
 			return_field_object.innerHTML = 'Invalid Email Address';
+
+			$('#Email_availability_result').css('color', 'red');
 			register_button_object.disable = true;
 			return false;
 		} else {
 			return_field_object.innerHTML = '';
 			register_button_object.disable = false;
+			var p = checkEmailAvailability(email_field_object.value, register_button_name, return_field_name);
+
+			return p;
 		}
 		
-	    return checkEmailAvailability(email_field_object.value, register_button_name, return_field_name);
+	    
+	    
+	    
 	}
 	
 	
 	function passwordVerification() {
+
+		 if (document.getElementById('password').value.length < 8) {
+        	document.getElementById('Confirm_password_result').innerHTML = 'Requirement: minimum 8 characters';
+        	register_button_object.disable = true;
+        	return false;
+        }
 		
         if ( document.getElementById('password').value != document.getElementById('confirmPassword').value ) {
         	document.getElementById('Confirm_password_result').innerHTML = 'Password mismatch';
         	$('#Confirm_password_result').css('color', 'red');
         	register_button_object.disable = true;
+        	return false;
         } else {
         	document.getElementById('Confirm_password_result').innerHTML = 'Passwords Match';
         	$('#Confirm_password_result').css('color', 'green');
         	register_button_object.disable = false;
+        	return true;
         }
         
-        if (document.getElementById('password').value.length < 8) {
-        	document.getElementById('Confirm_password_result').innerHTML = 'Requirement: minimum 8 characters';
-        	register_button_object.disable = true;
-        }
+       
 
 	}
+
+
+//function to check email availability
+function checkEmailAvailability(email, button, returnResult) {
+        //get the email
+
+		if (email.length < 6) { 
+			document.getElementById(returnResult).innerHTML = 'Min length is 6';
+			return false;
+		}
+		
+		
+        $.post("/session/checkForAvailableEmail", { 'email': email },
+            function(result, status) {
+        	if (result == 0) {  		
+                //show that the email is available
+            	document.getElementById(returnResult).innerHTML = 'Available';
+            	$('#' + returnResult).css('color', 'green');
+                document.getElementById(button).disabled = false;
+                ///alert(flag2)
+
+                //passwordVerification();
+
+                if(passwordVerification())
+                	document.getElementById('register-form').submit();
+
+                //alert(result)
+                
+                return true
+            } else {
+                //show that the email is NOT available
+            	document.getElementById(returnResult).innerHTML = 'Not Available';
+            	$('#' + returnResult).css('color', 'red');
+                document.getElementById(button).disabled = true;
+                return false;
+            }
+
+        });  
+} 
 
 	
 	require("/js/checkAvailability.js");
