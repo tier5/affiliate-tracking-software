9df4539161bdac391095fdddea6144bcc6a4ef0f
Gary Gipson <Gary Gipson>
2016-12-30 15:23:52 -0600

    BUGFIX:  Viral codes could potentially pick a deleted subscription

--- admin/app/controllers/SessionController.php
+++ admin/app/controllers/SessionController.php
@@ -119,7 +119,7 @@ class SessionController extends ControllerBase {
 
                 $objSuperUser = \Vokuro\Models\Users::findFirst("agency_id = {$objBusiness->parent_id} and role = 'Super Admin'");
 
-                $objSubscription = \Vokuro\Models\SubscriptionPricingPlan::findFirst("is_viral = 1 AND user_id = {$objSuperUser->id}");
+                $objSubscription = \Vokuro\Models\SubscriptionPricingPlan::findFirst("is_viral = 1 AND user_id = {$objSuperUser->id} AND name NOT LIKE 'deleted-%'");
                 if($objSubscription)
                     $subscription_id = $objSubscription->id;
                 $parent_id = $objBusiness->parent_id;
