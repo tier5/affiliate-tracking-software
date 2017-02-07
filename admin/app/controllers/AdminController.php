0bb0d4db3a450b584db6bb3976ce87da2ca2fd43
Adam McMahon <mcmahonempire@gmail.com>
2017-02-07 14:43:09 -0700

    REFACTOR: fix formatting

--- admin/app/controllers/AdminController.php
+++ admin/app/controllers/AdminController.php
@@ -18,9 +18,10 @@ use Phalcon\UserPlugin\Connectors\FacebookConnector;
 use Phalcon\Mvc\View;
 use Phalcon\Tag;
 
-class AdminController extends ControllerBase {
-
-    public function indexAction() {
+class AdminController extends ControllerBase
+{
+    public function indexAction()
+    {
         if (false === $this->auth->isUserSignedIn()) {
             $this->response->redirect(['action' => 'login']);
         }
@@ -30,7 +31,8 @@ class AdminController extends ControllerBase {
      * Login user
      * @return \Phalcon\Http\ResponseInterface
      */
-    public function loginAction() {
+    public function loginAction()
+    {
         if (true === $this->auth->isUserSignedIn()) {
             $this->response->redirect(array('action' => 'profile'));
         }
@@ -51,7 +53,8 @@ class AdminController extends ControllerBase {
      *
      * @return \Phalcon\Http\ResponseInterface
      */
-    public function signoutAction() {
+    public function signoutAction()
+    {
         $this->auth->remove();
         return $this->response->redirect('/', true);
     }
@@ -59,7 +62,8 @@ class AdminController extends ControllerBase {
     /**
      * Shows the forgot password form
      */
-    public function forgotPasswordAction() {
+    public function forgotPasswordAction()
+    {
         $form = new ForgotPasswordForm();
 
         if ($this->request->isPost()) {
@@ -94,21 +98,22 @@ class AdminController extends ControllerBase {
     /**
      * Reset pasword
      */
-    public function resetPasswordAction($code, $email) {
+    public function resetPasswordAction($code, $email)
+    {
         $resetPassword = UserResetPasswords::findFirstByCode($code);
 
         if (!$resetPassword) {
             $this->flash->error('Invalid or expired code');
             return $this->dispatcher->forward(array(
-                        'controller' => 'index',
-                        'action' => 'index'
+                'controller' => 'index',
+                'action' => 'index'
             ));
         }
 
         if ($resetPassword->getReset() <> 0) {
             return $this->dispatcher->forward(array(
-                        'controller' => 'user',
-                        'action' => 'login'
+                'controller' => 'user',
+                'action' => 'login'
             ));
         }
 
@@ -124,8 +129,8 @@ class AdminController extends ControllerBase {
             }
 
             return $this->dispatcher->forward(array(
-                        'controller' => 'index',
-                        'action' => 'index'
+                'controller' => 'index',
+                'action' => 'index'
             ));
         }
 
@@ -137,8 +142,8 @@ class AdminController extends ControllerBase {
         $this->flash->success('Please reset your password');
 
         return $this->dispatcher->forward(array(
-                    'controller' => 'user',
-                    'action' => 'changePassword'
+            'controller' => 'user',
+            'action' => 'changePassword'
         ));
     }
 
@@ -146,7 +151,8 @@ class AdminController extends ControllerBase {
      * Users must use this action to change its password
      *
      */
-    public function changePasswordAction() {
+    public function changePasswordAction()
+    {
         $form = new ChangePasswordForm();
 
         if ($this->request->isPost()) {
@@ -156,7 +162,7 @@ class AdminController extends ControllerBase {
                 }
             } else {
                 $user = $this->auth->getUser();
-                $_SESSION['password_save1']=$this->request->getPost('password');
+                $_SESSION['password_save1'] = $this->request->getPost('password');
                 $user->setPassword($this->security->hash($this->request->getPost('password')));
                 $user->setMustChangePassword(0);
 
@@ -182,23 +188,24 @@ class AdminController extends ControllerBase {
     /**
      * Confirms an e-mail, if the user must change its password then changes it
      */
-    public function confirmEmailAction($code) {
+    public function confirmEmailAction($code)
+    {
 
         $confirmation = \Vokuro\Models\EmailConfirmations::findFirstByCode($code);
 
         if (!$confirmation) {
             $this->flash->error('Invalid or expired code');
             return $this->dispatcher->forward(array(
-                        'controller' => 'index',
-                        'action' => 'index'
+                'controller' => 'index',
+                'action' => 'index'
             ));
         }
 
         if ($confirmation->isConfirmed() && $confirmation->user->mustChangePassword == 'N') {
             $this->flash->notice('This account is already activated. You can login.');
             return $this->dispatcher->forward(array(
-                        'controller' => 'user',
-                        'action' => 'login'
+                'controller' => 'user',
+                'action' => 'login'
             ));
         }
 
@@ -211,75 +218,87 @@ class AdminController extends ControllerBase {
             }
 
             return $this->dispatcher->forward(array(
-                        'controller' => 'index',
-                        'action' => 'index'
+                'controller' => 'index',
+                'action' => 'index'
             ));
         }
 
         $this->auth->authUserById($confirmation->user->getId());
-        //echo $confirmation->user->id;exit;
 
-
-            /*** agency information ***/
-            $_SESSION['confirm_user_id']=$confirmation->user->id;
-            $AgencyUser='';
+        /*** agency information ***/
+        $_SESSION['confirm_user_id'] = $confirmation->user->id;
+        $AgencyUser = '';
         $conditions = "agency_id = :agency_id:";
         $parameters = array("agency_id" => $confirmation->user->agency_id);
         $agency = Agency::findFirst(array($conditions, "bind" => $parameters));
         $Domain = $this->config->application->domain;
 
-        if($agency->parent_id > 0) {
-            $objParentAgency = \Vokuro\Models\Agency::findFirst("agency_id = {$agency->parent_id}");
+        if ($agency->parent_id > 0) {
+            $objParentAgency = \Vokuro\Models\Agency::findFirst(
+                "agency_id = {$agency->parent_id}"
+            );
 
-            $objParentAgencyInfo = \Vokuro\Models\Users::findFirst("agency_id = {$agency->parent_id} and is_employee=0");
-            if(!$objParentAgency->email_from_address && !$objParentAgency->custom_domain)
-                throw \Exception("Contact customer support.  Email configuration not setup correctly");
+            $objParentAgencyInfo = \Vokuro\Models\Users::findFirst(
+                "agency_id = {$agency->parent_id} and is_employee=0"
+            );
+            
+            if (!$objParentAgency->email_from_address && !$objParentAgency->custom_domain) {
+                throw \Exception(
+                    "Contact customer support.  Email configuration not setup correctly"
+                );
+            }
+
             $EmailFrom = $objParentAgency->email_from_address ?: "no-reply@{$objParentAgency->custom_domain}.{$Domain}";
             $EmailFromName = $objParentAgency->email_from_name ?: "";
-            $AgencyUser=$objParentAgencyInfo->name." ".$objParentAgencyInfo->last_name;
+            $AgencyUser = $objParentAgencyInfo->name . " " . $objParentAgencyInfo->last_name;
         }
 
-        if($agency->parent_id == \Vokuro\Models\Agency::BUSINESS_UNDER_RV)
-        {
+        if ($agency->parent_id == \Vokuro\Models\Agency::BUSINESS_UNDER_RV) {
             $EmailFrom = 'zacha@reviewvelocity.co';
             $EmailFromName = "Zacha Anderson";
         }
-        if($agency->parent_id == \Vokuro\Models\Agency::AGENCY) {
-            if(!$agency->email_from_address && !$agency->custom_domain)
-                throw \Exception("Contact customer support.  Email configuration not setup correctly");
+
+        if ($agency->parent_id == \Vokuro\Models\Agency::AGENCY) {
+            if (!$agency->email_from_address && !$agency->custom_domain) {
+                throw \Exception(
+                    "Contact customer support.  Email configuration not setup correctly"
+                );
+            }
+
             $EmailFrom = $agency->email_from_address ?: "no-reply@{$agency->custom_domain}.{$Domain}";
             $EmailFromName = $agency->email_from_name ?: "";
         }
-        if( $AgencyUser=='')
-        {
-             $AgencyUser=$agency->name;
+
+        if ($AgencyUser == ''){
+            $AgencyUser = $agency->name;
         }
        
 
         $conditions = "agency_id = :agency_id:";
         $parameters = array("agency_id" => $agency->parent_id);
-        $parent_agency = Agency::findFirst(array($conditions, "bind" => $parameters));
+        $parent_agency = Agency::findFirst(
+            array($conditions, "bind" => $parameters)
+        );
 
-        $AgencyName=$parent_agency->name;
+        $AgencyName = $parent_agency->name;
         $Domain = $this->config->application->domain;
 
-
-        $_SESSION['Agencyname_log']=$AgencyName;
-        $_SESSION['AgencyUser_log']=$AgencyUser;
-        $_SESSION['EmailFrom_log']=$EmailFrom;
-        $_SESSION['EmailFromName_log']=$EmailFromName;
-        $_SESSION['toemail_log']=$confirmation->user->email;
-        $_SESSION['name_log']=$confirmation->user->name;
-            /*** agency information ***/
+        $_SESSION['Agencyname_log'] = $AgencyName;
+        $_SESSION['AgencyUser_log'] = $AgencyUser;
+        $_SESSION['EmailFrom_log'] = $EmailFrom;
+        $_SESSION['EmailFromName_log'] = $EmailFromName;
+        $_SESSION['toemail_log'] = $confirmation->user->email;
+        $_SESSION['name_log'] = $confirmation->user->name;
+        /*** agency information ***/
 
         /*** Feedback form ***/
-               $publicUrl="http://{$Domain}";
-                    $code=$confirmation->user->id."-".$confirmation->user->name;
-                    $link=$publicUrl.'/link/createlink/'.base64_encode($code);
-                    $feed_back_email=$confirmation->user->email;
-                    $feed_back_subj='Feedback Form';
-                    $feed_back_body='Hi '.$confirmation->user->name.',';
-                    $feed_back_body=$feed_back_body.'<p>Thank you for activating your account, we have created a mobile landing page so that you can request feedback from your customers in person from your mobile phone. 
+        $publicUrl = "http://{$Domain}";
+        $code = $confirmation->user->id . "-" . $confirmation->user->name;
+        $link = $publicUrl . '/link/createlink/' . base64_encode($code);
+        $feed_back_email = $confirmation->user->email;
+        $feed_back_subj = 'Feedback Form';
+        $feed_back_body = 'Hi ' . $confirmation->user->name . ',';
+        $feed_back_body .= '<p>Thank you for activating your account, we have created a mobile landing page so that you can request feedback from your customers in person from your mobile phone. 
                         </p>
 
                         <p>Click on the link below and add the the page to your home screen so that you can easily access this page. This link is customized to you so that all feedback and reviews will be tracked back to your account. 
@@ -287,51 +306,51 @@ class AdminController extends ControllerBase {
 
                         <p>The best practices is to ask your customer for feedback right after you have completed the services for them. We recommend that you ask them to please leave a review on one of the sites we suggest and to mention your name in the review online.</p>';
 
-                        $feed_back_body=$feed_back_body.'<a href="'.$link.'">Personalized Feedback Form - Click Here </a>
+        $feed_back_body .= '<a href="' . $link . '">Personalized Feedback Form - Click Here </a>
                         <p>Do not give this link out to any one else it is a personalized link for you and will track all your feedback requests. Each employee has their own personalized feedback form. </p>';
 
-                        /*** login information ****/
-                          if($_SESSION['password_save1'])
-                        {   
-                             $feed_back_body=$feed_back_body.'Login Details:</br>';
-                             $feed_back_body=$feed_back_body.'<p>Please view the Login Credentials Below: </p>';
-                             $feed_back_body=$feed_back_body.'Login URL:';
-                             $feed_back_body=$feed_back_body."<br>Login Email: ".$feed_back_email."<br>";
-                             $feed_back_body=$feed_back_body."Login Password: ". $_SESSION['password_save1']."<br>";
-                             
-                        }
+        /*** login information ****/
+        if ($_SESSION['password_save1']) {   
+            $feed_back_body .= 'Login Details:</br>';
+            $feed_back_body .= '<p>Please view the Login Credentials Below: </p>';
+            $feed_back_body .= 'Login URL:';
+            $feed_back_body .= "<br>Login Email: " . $feed_back_email . "<br>";
+            $feed_back_body .= "Login Password: " . $_SESSION['password_save1'] . "<br>";
+        }
 
-                        $feed_back_body=$feed_back_body."<p>Looking forward to helping you build a strong online reputation.</p>";                         
+        $feed_back_body .= "<p>Looking forward to helping you build a strong online reputation.</p>";
 
-                        
-                        /*** login information ****/
+        /*** login information ****/
 
-                        $feed_back_body=$feed_back_body."<br>".$AgencyUser."<br>".$AgencyName;
-                       /* $Mail = $this->getDI()->getMail();
-                        $Mail->setFrom($EmailFrom,$EmailFromName);
-                        $Mail->send($feed_back_email, $feed_back_subj, '', '', $feed_back_body);*/
+        $feed_back_body .= "<br>" . $AgencyUser . "<br>" . $AgencyName;
+        /* $Mail = $this->getDI()->getMail();
+        $Mail->setFrom($EmailFrom,$EmailFromName);
+        $Mail->send($feed_back_email, $feed_back_subj, '', '', $feed_back_body);*/
 
-                        $_SESSION['password_save1']='';
-                        /*** Feedback form ***/
-
-                        
-
+        $_SESSION['password_save1'] = '';
+        /*** Feedback form ***/
 
         if ($confirmation->user->mustChangePassword == 'Y') {
-            $this->flash->success('The email was successfully confirmed. Now you must change your password');
-            return $this->response->redirect($this->_activeLanguage . '/session/changePassword');
+            $this->flash->success(
+                'The email was successfully confirmed. Now you must change your password'
+            );
+            
+            return $this->response->redirect(
+                $this->_activeLanguage . '/session/changePassword'
+            );
         }
 
         $this->flash->success('The email was successfully confirmed');
+
         return $this->dispatcher->forward(array(
-                    'controller' => 'user',
-                    'action' => 'login'
+            'controller' => 'user',
+            'action' => 'login'
         ));
         //return $this->response->redirect($this->_activeLanguage.'/user/login');
     }
 
-    public function profileAction() {
+    public function profileAction()
+    {
 
     }
-
 }
