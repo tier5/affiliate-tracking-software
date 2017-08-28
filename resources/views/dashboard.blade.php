2ae1c45fbdf817b86604fd20680b355a5de3e211
Tier5 LLC <github@tier5.us>
2017-08-28 20:00:04 +0530

    Admin Dashboard Filteration v1.6

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -72,6 +72,10 @@
                     <!-- /.info-box -->
                 </div>
                 <!-- /.col -->
+
+                <!-- fix for small devices only -->
+                <div class="clearfix visible-sm-block"></div>
+
                 <div class="col-md-3 col-sm-6 col-xs-12">
                     <div class="info-box">
                         <span class="info-box-icon bg-red"><i class="fa fa-google-plus"></i></span>
@@ -85,21 +89,6 @@
                     <!-- /.info-box -->
                 </div>
                 <!-- /.col -->
-                <!-- fix for small devices only -->
-                <div class="clearfix visible-sm-block"></div>
-
-                <div class="col-md-3 col-sm-6 col-xs-12">
-                    <div class="info-box">
-                        <span class="info-box-icon bg-green"><i class="ion ion-ios-cart-outline"></i></span>
-
-                        <div class="info-box-content">
-                            <span class="info-box-text">Customers</span>
-                            <span class="info-box-number">{{ $sales->count() }}</span>
-                        </div>
-                        <!-- /.info-box-content -->
-                    </div>
-                    <!-- /.info-box -->
-                </div>
 
                 <!-- /.col -->
                 <div class="col-md-3 col-sm-6 col-xs-12">
@@ -115,6 +104,22 @@
                     <!-- /.info-box -->
                 </div>
                 <!-- /.col -->
+
+                <!-- fix for small devices only -->
+                <div class="clearfix visible-sm-block"></div>
+
+                <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="info-box">
+                        <span class="info-box-icon bg-green"><i class="ion ion-ios-cart-outline"></i></span>
+
+                        <div class="info-box-content">
+                            <span class="info-box-text">Customers</span>
+                            <span class="info-box-number">{{ $sales->count() }}</span>
+                        </div>
+                        <!-- /.info-box-content -->
+                    </div>
+                    <!-- /.info-box -->
+                </div>
             </div>
             <!-- /.row -->
 
