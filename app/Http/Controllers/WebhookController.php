0096a7c94b1b74123c1ed79fd54be6cee0320ae0
Tier5 LLC <github@tier5.us>
2017-09-14 17:25:33 +0530

    Stripe intregration V0.4

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -3,13 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Cartalyst\Stripe\WebhookController as BaseWebhookController;
 use Illuminate\Support\Facades\Log;
 
-class WebhookController extends BaseWebhookController
+class WebhookController extends Controller
 {
     public function stripeCallBack($charge)
     {
+        // Set your secret key: remember to change this to your live secret key in production
+        // See your keys here: https://dashboard.stripe.com/account/apikeys
+        \Stripe\Stripe::setApiKey("sk_test_Lf7l6YvX9WNgphuWzg3ntDJ5");
+
+        // You can find your endpoint's secret in your webhook settings
+        $endpoint_secret = "whsec_uB7FqnVseC2gnbyZih5XHg8OUkTdMdho";
+        $payload = @file_get_contents("php://input");
+        $sig_header = $_SERVER["HTTP_STRIPE_SIGNATURE"];
+        $event = null;
+        try {
+            $event = \Stripe\Webhook::constructEvent(
+                $payload, $sig_header, $endpoint_secret
+            );
+        } catch(\UnexpectedValueException $e) {
+            // Invalid payload
+            http_response_code(400); // PHP 5.4 or greater
+            exit();
+        } catch(\Stripe\Error\SignatureVerification $e) {
+            // Invalid signature
+            http_response_code(400); // PHP 5.4 or greater
+            exit();
+        }
+        // Do something with $event
         Log::info('here');
+        http_response_code(200); // PHP 5.4 or greater
     }
 }
